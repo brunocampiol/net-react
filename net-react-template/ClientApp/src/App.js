b57875fa5d50194a4f4ae426383df043ecc1bb85
Bruno Paes <bruno.paes@adp.com>
2022-01-12 13:27:25 -0300

    adding about component

--- net-react-template/ClientApp/src/App.js
+++ net-react-template/ClientApp/src/App.js
@@ -6,17 +6,19 @@ import { FetchData } from './components/FetchData';
 import { Counter } from './components/Counter';
 
 import './custom.css'
+import { About } from './components/About';
 
 export default class App extends Component {
-  static displayName = App.name;
+    static displayName = App.name;
 
-  render () {
-    return (
-      <Layout>
-        <Route exact path='/' component={Home} />
-        <Route path='/counter' component={Counter} />
-        <Route path='/fetch-data' component={FetchData} />
-      </Layout>
-    );
-  }
+    render() {
+        return (
+            <Layout>
+                <Route exact path='/' component={Home} />
+                <Route path='/about' component={About} />
+                <Route path='/counter' component={Counter} />
+                <Route path='/fetch-data' component={FetchData} />
+            </Layout>
+        );
+    }
 }
---
+++ net-react-template/ClientApp/src/components/About.js
@@ -0,0 +1,13 @@
+﻿import React, { Component } from 'react';
+
+export class About extends Component {
+
+    render() {
+        return (
+            <div>
+                <h1>About</h1>
+                <p>This is a simple about page</p>
+            </div>
+        );
+    }
+}
